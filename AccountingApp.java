139c53f5a9af84d5eb2b9bfbdd4ed2bad28de056
triple486 <dreamingkt12@gmail.com>
2020-10-23 19:20:02 +0900

    added comment for Value of supply, VAT, and total

--- AccountingApp.java
+++ AccountingApp.java
@@ -2,6 +2,7 @@
 public class AccountingApp {
 
 	public static void main(String[] args) {
+		//added Value of supply, VAT, and total
 		System.out.println("Value of supply : "+10000.0);
 		System.out.println("VAT : "+ (10000.0*0.1) );
 		System.out.println("Total : "+ (10000.0 + 10000.0*0.1) );
