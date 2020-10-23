8a29011d16334bca961320b8104315fb165dacb2
triple486 <dreamingkt12@gmail.com>
2020-10-23 19:15:09 +0900

    added value of supply, VAT, and total

--- AccountingApp.java
+++ AccountingApp.java
@@ -2,8 +2,9 @@
 public class AccountingApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Value of supply : "+10000.0);
+		System.out.println("VAT : "+ (10000.0*0.1) );
+		System.out.println("Total : "+ (10000.0 + 10000.0*0.1) );
 	}
 
 }
