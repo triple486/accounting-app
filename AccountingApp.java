d2dfc33b12ff16d1f581e38cf0b0bb78ed8c3d3f
triple486 <dreamingkt12@gmail.com>
2020-10-24 15:25:28 +0900

    added expense, income, dividend 1,2,3

--- AccountingApp.java
+++ AccountingApp.java
@@ -6,6 +6,11 @@ public class AccountingApp {
 		System.out.println("Value of supply : "+10000.0);
 		System.out.println("VAT : "+ (10000.0*0.1) );
 		System.out.println("Total : "+ (10000.0 + 10000.0*0.1) );
+		System.out.println("Expense : "+ (10000.0*0.3) );
+		System.out.println("Income : "+ (10000.0 - 10000.0*0.3) );
+		System.out.println("Dividend 1 : "+ (10000.0 - 10000.0*0.3) * 0.5);
+		System.out.println("Dividend 2 : "+ (10000.0 - 10000.0*0.3) * 0.3);
+		System.out.println("Dividend 3 : "+ (10000.0 - 10000.0*0.3) * 0.2);
 	}
 
 }
